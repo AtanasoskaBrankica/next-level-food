505b8d44a15a174b86c93ca6493d30b5fc3a22bb
Brankica Atanasoska <atanasoskabrankica98@gmail.com>
2024-02-21 11:45:13 +0100

    Moved Project to Git Hub Repository

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>An error occured!</h1>
+      <p>Failed to fetch maeal data. Please try again later.</p>
+    </main>
+  );
+}
